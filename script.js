9ba105bc96f04b761f0a75f4f10a49928a5a37ab
soheldev.codes <soheldev.codes@gmail.com>
2026-02-22 22:57:46 +0600

    Empty Design and data Generatre

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
+// Data
+let allJobs = [
+    { id: 1, company: "Google", position: "Frontend Developer", location: "Remote", type: "Full-Time", salary: "$4000", description: "Develop modern UI using React.", status: "" },
+    { id: 2, company: "Amazon", position: "Backend Developer", location: "USA", type: "Full-Time", salary: "$4500", description: "Work with Node.js and databases.", status: "" },
+    { id: 3, company: "Microsoft", position: "UI Designer", location: "Canada", type: "Contract", salary: "$3500", description: "Design user-friendly interfaces.", status: "" },
+    { id: 4, company: "Tesla", position: "Software Engineer", location: "USA", type: "Full-Time", salary: "$5000", description: "Build scalable applications.", status: "" },
+    { id: 5, company: "Meta", position: "Product Manager", location: "Remote", type: "Full-Time", salary: "$4800", description: "Manage product lifecycle.", status: "" },
+    { id: 6, company: "Spotify", position: "Mobile Developer", location: "Sweden", type: "Full-Time", salary: "$4200", description: "Build Android/iOS apps.", status: "" },
+    { id: 7, company: "Airbnb", position: "QA Engineer", location: "UK", type: "Part-Time", salary: "$3000", description: "Test web applications.", status: "" },
+    { id: 8, company: "Netflix", position: "DevOps Engineer", location: "Remote", type: "Full-Time", salary: "$4700", description: "Manage cloud infrastructure.", status: "" }
+];
+
+
+function updateDashboard() {
+    document.getElementById("totalJobCount").innerText = allJobs.length;
+    document.getElementById("interviewCount").innerText = allJobs.filter(j => j.status === "Interview").length;
+    document.getElementById("rejectedCount").innerText = allJobs.filter(j => j.status === "Rejected").length;
+}
+
+
 
 
 // Default Tab Button Filter 
-let tabBtn = "All"
+let currentTab = "All"
 
 function tabButton(event, id) {
     
-    tabBtn = id;
+    currentTab = id;
 
     const tabBtns = document.querySelectorAll(".tab-btn");
 
@@ -17,3 +37,7 @@ function tabButton(event, id) {
     event.currentTarget.classList.remove("bg-gray-200", "text-black");
     event.currentTarget.classList.add("bg-black", "text-white");
 }
+
+
+
+
