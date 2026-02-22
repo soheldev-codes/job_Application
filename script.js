d63b0f5c997677d1c0e83735dbc541f386832bbf
soheldev.codes <soheldev.codes@gmail.com>
2026-02-22 22:48:56 +0600

    Filter Button design And  Dom implement Active color Add

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Connected")
+
+
+// Default Tab Button Filter 
+let tabBtn = "All"
+
+function tabButton(event, id) {
+    
+    tabBtn = id;
+
+    const tabBtns = document.querySelectorAll(".tab-btn");
+
+    tabBtns.forEach(btn => {
+        btn.classList.remove("bg-black", "text-white");
+        btn.classList.add("bg-gray-200", "text-black");
+    });
+
+    event.currentTarget.classList.remove("bg-gray-200", "text-black");
+    event.currentTarget.classList.add("bg-black", "text-white");
+}
