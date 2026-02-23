e83089cb6093c498b4be91a1a2c07f4cf2a8a256
soheldev.codes <soheldev.codes@gmail.com>
2026-02-23 20:50:03 +0600

    Render job and functionality done

--- script.js
+++ script.js
@@ -11,14 +11,6 @@ let allJobs = [
 ];
 
 
-function updateDashboard() {
-    document.getElementById("totalJobCount").innerText = allJobs.length;
-    document.getElementById("interviewCount").innerText = allJobs.filter(j => j.status === "Interview").length;
-    document.getElementById("rejectedCount").innerText = allJobs.filter(j => j.status === "Rejected").length;
-}
-
-
-
 
 // Default Tab Button Filter 
 let currentTab = "All"
@@ -26,6 +18,7 @@ let currentTab = "All"
 function tabButton(event, id) {
     
     currentTab = id;
+    console.log(currentTab);
 
     const tabBtns = document.querySelectorAll(".tab-btn");
 
@@ -36,7 +29,116 @@ function tabButton(event, id) {
 
     event.currentTarget.classList.remove("bg-gray-200", "text-black");
     event.currentTarget.classList.add("bg-black", "text-white");
+
+     renderJobs();
 }
+
+
+
+
+function updateDashboard() {
+    document.getElementById("totalJobCount").innerText = allJobs.length;
+    document.getElementById("interviewCount").innerText = allJobs.filter(j => j.status === "Interview").length;
+    document.getElementById("rejectedCount").innerText = allJobs.filter(j => j.status === "Rejected").length;
+}
+
+function renderJobs() {
+
+    updateDashboard();
+    const container = document.getElementById("jobsContainer")
+    const emptyState = document.getElementById("emptyState")
+
+    container.innerHTML = "";
+
+    let filterJobs;
+
+    if (currentTab === "All") {
+        filterJobs = allJobs;
+    } else {
+        filterJobs = allJobs.filter(job => job.status === currentTab);
+    }
+
+
+
+     document.getElementById("tabCount").innerText = filterJobs.length + " Jobs";
+
+    // No Jobs Then Show Empty State Design
+     if (filterJobs.length === 0) {
+        emptyState.classList.remove("hidden");
+        return;
+    } else {
+        emptyState.classList.add("hidden");
+    }
+
+
+    filterJobs.forEach(job => {
+        const card = document.createElement("div");
+        card.className = "bg-white mt-5 p-5 rounded-xl shadow sm:flex  justify-between";
+
+        card.innerHTML = `
+            <div>
+            <h3 class="text-lg font-bold">${job.company}</h3>
+            <p class="text-sm text-gray-500">${job.position}</p>
+            <p class="text-sm mt-2">${job.location} • ${job.type}</p>
+            <p class="text-sm font-semibold mt-2">${job.salary}</p>
+            ${job.status ? `
+            <span class="inline-block mt-3 mb-2 px-3 py-1 text-xs font-semibold rounded
+                ${job.status === "Interview"
+                    ? "bg-blue-100 text-blue-700"
+                    : "bg-red-100 text-red-700"}">
+                ${job.status}
+            </span>` : ""}
+            <p class="text-gray-600 text-sm mt-2">${job.description}</p>
+
+            <div class="flex gap-2 mt-4">
+                <button onclick="markInterview(${job.id})"
+                    class="px-3 py-1 text-sm rounded ${job.status === "Interview" ? "bg-blue-600 text-white" : "bg-gray-200"}">
+                    Interview
+                </button>
+
+                <button onclick="markRejected(${job.id})"
+                    class="px-3 py-1 text-sm rounded ${job.status === "Rejected" ? "bg-red-600 text-white" : "bg-gray-200"}">
+                    Rejected
+                </button>
+            </div>
+            </div>
+            <div class="mt-5 sm:m-px">
+                <button onclick="deleteJob(${job.id})"
+                  class="px-3 py-1 text-sm rounded bg-black text-white ">
+                    Delete
+                </button>
+            </div>
+        `;
+
+        container.appendChild(card);
+    });
+
+    updateDashboard();
+
+}
+
+
+function markInterview(id) {
+    const job = allJobs.find(j => j.id === id);
+    job.status = job.status === "Interview" ? "" : "Interview";
+    renderJobs();
+}
+
+function markRejected(id) {
+    const job = allJobs.find(j => j.id === id);
+    job.status = job.status === "Rejected" ? "" : "Rejected";
+    renderJobs();
+}
+
+function deleteJob(id) {
+    allJobs = allJobs.filter(j => j.id !== id);
+    renderJobs();
+}
+
+
+
+ renderJobs()
+
 
 
 
